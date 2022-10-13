dd512d515608d930084de08fef4419fcd83ec80b
adtak <adtak62@gmail.com>
2022-10-13 09:51:26 +0000

    format

--- mlops_cdk_sample/sample_app_stack.py
+++ mlops_cdk_sample/sample_app_stack.py
@@ -1,5 +1,4 @@
 from aws_cdk import Duration, Stack
-from aws_cdk import aws_iam as iam
 from aws_cdk import aws_sns as sns
 from aws_cdk import aws_sns_subscriptions as subs
 from aws_cdk import aws_sqs as sqs
