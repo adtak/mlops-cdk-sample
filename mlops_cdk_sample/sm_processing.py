8940b8243685fc2517efc3e0bfe1ef49b5d16d41
adtak <adtak62@gmail.com>
2022-11-02 12:02:40 +0900

    refactoring sm stack

--- mlops_cdk_sample/sm_processing.py
+++ mlops_cdk_sample/sm_processing.py
@@ -1,3 +1,5 @@
+from typing import Any, Dict
+
 import aws_cdk.aws_stepfunctions as sfn
 from aws_cdk import Duration, Stack
 from constructs import Construct
@@ -6,51 +8,12 @@ from constructs import Construct
 class SampleSageMakerProcessingStack(Stack):
     def __init__(self, scope: Construct, construct_id: str, **kwargs) -> None:
         super().__init__(scope, construct_id, **kwargs)
-        image_uri = kwargs["image_uri"]
-        input_s3_uri = kwargs["input_s3_uri"]
-        output_s3_uri = kwargs["output_s3_uri"]
-        role_arn = ""
-        state_json = {
-            "Type": "Task",
-            "Resource": "arn:aws:states:::aws-sdk:sagemaker:createProcessingJob",
-            "Parameters": {
-                "AppSpecification": {"ImageUri.$": image_uri},
-                "ProcessingJobName": "SamplePreProcessingJob",
-                "ProcessingResources": {
-                    "ClusterConfig": {
-                        "InstanceCount": 1,
-                        "InstanceType": "ml.t3.medium",
-                        "VolumeSizeInGB": 1,
-                    }
-                },
-                "ProcessingInputs": [
-                    {
-                        "InputName": "SamplePreProcessingJobInput",
-                        "S3Input": {
-                            "LocalPath": "/opt/ml/processing/",
-                            "S3DataType": "S3Prefix",
-                            "S3InputMode": "File",
-                            "S3Uri": input_s3_uri,
-                        },
-                    }
-                ],
-                "ProcessingOutputConfig": {
-                    "Outputs": [
-                        {
-                            "OutputName": "SamplePreProcessingJobOutput",
-                            "S3Output": {
-                                "LocalPath": "/opt/ml/processing/",
-                                "S3UploadMode": "EndOfJob",
-                                "S3Uri": output_s3_uri,
-                            },
-                        }
-                    ],
-                },
-                "RoleArn.$": role_arn,
-            },
-        }
         preprocess_step = sfn.CustomState(
-            self, "SamplePreProcessingTask", state_json=state_json
+            self,
+            "SamplePreProcessingTask",
+            state_json=_create_processing_job_state(
+                kwargs["image_uri"], kwargs["input_s3_uri"], kwargs["output_s3_uri"]
+            ),
         )
         success_step = sfn.Succeed(self, id="Succeded")
         definition = preprocess_step.next(success_step)
@@ -60,3 +23,47 @@ class SampleSageMakerProcessingStack(Stack):
             definition=definition,
             timeout=Duration.minutes(5),
         )
+
+
+def _create_processing_job_state(
+    image_uri, input_s3_uri, output_s3_uri, role_arn
+) -> Dict[str, Any]:
+    return {
+        "Type": "Task",
+        "Resource": "arn:aws:states:::aws-sdk:sagemaker:createProcessingJob",
+        "Parameters": {
+            "AppSpecification": {"ImageUri.$": image_uri},
+            "ProcessingJobName": "SamplePreProcessingJob",
+            "ProcessingResources": {
+                "ClusterConfig": {
+                    "InstanceCount": 1,
+                    "InstanceType": "ml.t3.medium",
+                    "VolumeSizeInGB": 1,
+                }
+            },
+            "ProcessingInputs": [
+                {
+                    "InputName": "SamplePreProcessingJobInput",
+                    "S3Input": {
+                        "LocalPath": "/opt/ml/processing/",
+                        "S3DataType": "S3Prefix",
+                        "S3InputMode": "File",
+                        "S3Uri": input_s3_uri,
+                    },
+                }
+            ],
+            "ProcessingOutputConfig": {
+                "Outputs": [
+                    {
+                        "OutputName": "SamplePreProcessingJobOutput",
+                        "S3Output": {
+                            "LocalPath": "/opt/ml/processing/",
+                            "S3UploadMode": "EndOfJob",
+                            "S3Uri": output_s3_uri,
+                        },
+                    }
+                ],
+            },
+            "RoleArn.$": role_arn,
+        },
+    }
