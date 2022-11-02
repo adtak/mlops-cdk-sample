bc671f0fe0d7cbb84c67a43719b4dc76d3f0c9b2
adtak <adtak62@gmail.com>
2022-11-02 15:03:06 +0900

    some fix

--- docker/preprocess.py
+++ docker/preprocess.py
@@ -5,7 +5,7 @@ def main():
     with open("./input.csv") as f:
         reader = csv.reader(f)
         input = [i for i in reader]
-    output = [i + "_out" for i in input]
+    output = ["".join(i) for i in input]
     with open("./output.csv", "w") as f:
         writer = csv.writer(f)
         writer.writerows(output)
