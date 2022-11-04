093d0813ccab2993c803d5d2cf68037337ae6973
adtak <adtak62@gmail.com>
2022-11-04 10:56:21 +0900

    update sample docker image

--- docker/preprocess.py
+++ docker/preprocess.py
@@ -1,14 +1,18 @@
 import csv
+import os
 
 
 def main():
-    with open("./input/input.csv") as f:
+    print("Start preprocessing.")
+    with open("./input.csv") as f:
         reader = csv.reader(f)
         input = [i for i in reader]
     output = ["".join(i) for i in input]
+    os.makedirs("output")
     with open("./output/output.csv", "w") as f:
         writer = csv.writer(f)
         writer.writerows(output)
+    print("Finish preprocessing.")
 
 
 if __name__ == "__main__":
