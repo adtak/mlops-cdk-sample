ec2f324806d4c42e5b11558f5715c969bb9f8a88
adtak <adtak62@gmail.com>
2022-11-04 14:22:47 +0900

    update docker image

--- docker/processing_script.py
+++ docker/processing_script.py
@@ -4,17 +4,17 @@ import pathlib
 
 
 def main():
-    print("Start preprocessing.")
+    print("Start processing.")
     with open("/opt/ml/processing/input/input.csv") as f:
         reader = csv.reader(f)
         input = [i for i in reader]
-    output = ["".join(i) for i in input]
+    print(f"Input: {input}")
     output_dir = pathlib.Path("/opt/ml/processing/output/")
     os.makedirs(output_dir, exist_ok=True)
     with open(output_dir / "output.csv", "w") as f:
         writer = csv.writer(f)
-        writer.writerows(output)
-    print("Finish preprocessing.")
+        writer.writerows(input)
+    print("Finish processing.")
 
 
 if __name__ == "__main__":
