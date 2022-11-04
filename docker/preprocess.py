9969ab9fec6eee8feb7f00f589ac98b9b4b1d22c
adtak <adtak62@gmail.com>
2022-11-04 13:51:58 +0900

    update sample doker image

--- docker/preprocess.py
+++ docker/preprocess.py
@@ -1,15 +1,17 @@
 import csv
 import os
+import pathlib
 
 
 def main():
     print("Start preprocessing.")
-    with open("./input.csv") as f:
+    with open("/opt/ml/processing/input/input.csv") as f:
         reader = csv.reader(f)
         input = [i for i in reader]
     output = ["".join(i) for i in input]
-    os.makedirs("output")
-    with open("./output/output.csv", "w") as f:
+    output_dir = pathlib.Path("/opt/ml/processing/output/")
+    os.makedirs(output_dir, exist_ok=True)
+    with open(output_dir / "output.csv", "w") as f:
         writer = csv.writer(f)
         writer.writerows(output)
     print("Finish preprocessing.")
